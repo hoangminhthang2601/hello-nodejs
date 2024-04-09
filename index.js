771ceab976ba60e68a3a61a6d82777c72079acac
Toan.Hoang <minhtoandut96@gmail.com>
2024-04-09 11:03:56 +0700

    change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.get('/', function(req, res) {   res.send('hello node'); });
+app.get('/', function(req, res) {   res.send('hello world'); });
 app.listen(3000,function() {  console.log('example listening on port 3000'); });
